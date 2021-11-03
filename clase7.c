0c6d97f37c514262736ce3b9370b5c105d57de22
resnonverba <72408060+resnonverba@users.noreply.github.com>
2021-11-03 01:02:52 +0100

    Add files via upload

---
+++ clase7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    int random;
+    srand(time(0));
+    random = rand() - RAND_MAX / 2;
+    if (random > 0)
+        printf("Es positivo: %d",random);
+    else if (random == 0)
+    {
+        printf("Es cero: %d", random);
+    }
+    else 
+    {
+        printf("Negativo");
+    }
+    return 0;
+}
+
